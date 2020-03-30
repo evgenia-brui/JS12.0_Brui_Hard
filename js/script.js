80837436fd240643a15ca10e9446beda0467005d
Evgenia Brui <j-lp@mail.ru>
2020-03-30 23:21:43 +0300

    Новый js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-const num = 266219;
+const lang = 'ru'; // 'ru' or 'en'
+const week_ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week_en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-const num_multiply = String(num).split('').reduce((multiply, current) => multiply * current, 1);
-console.log('num_multiply = ' + num_multiply);
+console.info('Дни недели if:');
+if (lang == 'ru') {
+    console.log(week_ru);
+} else if (lang == 'en') {
+    console.log(week_en);
+}
 
-const num_pow = num_multiply ** 3;
-console.log('num_pow = ' + num_pow);
+console.info('\nДни недели switch:');
+switch (lang) {
+    case 'ru':
+        console.log(week_ru);
+        break;
+    case 'en':
+        console.log(week_en);
+        break;
+}
 
-alert('Первые 2 цифры полученного числа: ' + String(num_pow).slice(0, 2));
+const langs = ['ru', 'en'];
+const lang_id = langs.indexOf(lang);
+const week_arr = [
+    week_ru,
+    week_en
+];
+console.info('\nДни недели из массива:');
+console.log(week_arr[lang_id]);
+
+const namePerson = 'Максим';
+const positionPerson = namePerson == 'Артем' ? 'директор' : namePerson == 'Максим' ? 'преподаватель' : 'студент';
+console.log('\n' + namePerson + ' - ' + positionPerson);
