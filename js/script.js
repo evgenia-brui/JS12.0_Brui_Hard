657babc22e3fda863e47d0c48b6f96eeded9037a
Evgenia Brui <j-lp@mail.ru>
2020-04-08 20:21:42 +0300

    Вывела на страницу

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const MINUTES_STRING = ['минута', 'минуты', 'минут'];
 const SECONDS_STRING = ['секунда', 'секунды', 'секунд'];
 const DAYS = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+const howTime1 = document.querySelector('.how-time-1');
+const howTime2 = document.querySelector('.how-time-2');
 
 function declOfNum(n, titles, from) {
     return n + ' ' + titles[from ? n % 10 === 1 && n % 100 !== 11 ? 1 : 2 :
@@ -16,6 +18,8 @@ function addZero(n) {
 
 let clock = setInterval(() => {
     const date = new Date();
-    console.log(`Сегодня ${DAYS[date.getDay()]}, ${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()} года, ${declOfNum(date.getHours(), HOURS_STRING)} ${declOfNum(date.getMinutes(), MINUTES_STRING)} ${declOfNum(date.getSeconds(), SECONDS_STRING)}`);
-    console.log(`${addZero(date.getDate())}.${addZero(date.getMonth()+1)}.${date.getFullYear()} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`);
+    howTime1.textContent = `Сегодня ${DAYS[date.getDay()]},
+    ${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()} года,
+    ${declOfNum(date.getHours(), HOURS_STRING)} ${declOfNum(date.getMinutes(), MINUTES_STRING)} ${declOfNum(date.getSeconds(), SECONDS_STRING)}`;
+    howTime2.textContent = `${addZero(date.getDate())}.${addZero(date.getMonth()+1)}.${date.getFullYear()} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
 }, 1000);
